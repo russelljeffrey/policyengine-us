c704b811607d369c856f81fabf22471fecd826e2
Russell Jeffrey <71146371+russelljeffrey@users.noreply.github.com>
2023-10-14 04:13:56 +0000

    wv variable file updated

--- policyengine_us/variables/gov/states/wv/tax/income/exemptions/wv_low_income_exemption.py
+++ policyengine_us/variables/gov/states/wv/tax/income/exemptions/wv_low_income_exemption.py
@@ -5,10 +5,16 @@ class wv_low_income_exemption(Variable):
     value_type = float
     entity = TaxUnit
     label = "West Virginia low income exemption"
-    defined_for = StateCode.WV
     unit = USD
     definition_period = YEAR
-    reference = "https://code.wvlegislature.gov/11-21-10/"
+    defined_for = StateCode.WV
 
     def formula(tax_unit, period, parameters):
+        # First get their filing status.
+        filing_status = tax_unit("filing_status", period)
+
+        # Then get the WV Exemptions part of the parameter tree.
         p = parameters(period).gov.states.wv.tax.income.exemptions.low_income
+
+        # Get their regular exemption amount based on their filing status.
+        return p.max_amount[filing_status]
